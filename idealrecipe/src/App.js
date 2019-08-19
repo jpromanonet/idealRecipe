5fea286e6037e2bb254fe7e1ee8d2bdb8f2c83f5
jpromanonet <programming@jpromano.net>
2019-08-19 18:24:56 -0300

    auth api added

--- idealrecipe/src/App.js
+++ idealrecipe/src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello react!</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const APP_ID = "d3fe2ead";
+  const APP_KEY = "62f4a658cef756d9b125eec16d55db17";
+
+  return (
+    <div className="App">
+      <h1>Hello React!</h1>
+    </div>
+  );
+};
 
 export default App;
