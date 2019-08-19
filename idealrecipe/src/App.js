07ea86248f2a3c77258805f8891ab2aa9c55d946
jpromanonet <programming@jpromano.net>
2019-08-19 18:34:59 -0300

    useEffect added

--- idealrecipe/src/App.js
+++ idealrecipe/src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  // API auth
   const APP_ID = "d3fe2ead";
   const APP_KEY = "62f4a658cef756d9b125eec16d55db17";
 
+  // API Search
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  // useEffect function
 
   return (
     <div className="App">
