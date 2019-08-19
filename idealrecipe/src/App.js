b2cd4460fdaebef5083318897b66c2f7d3661e4f
jpromanonet <programming@jpromano.net>
2019-08-19 18:32:35 -0300

    buttons added

--- idealrecipe/src/App.js
+++ idealrecipe/src/App.js
@@ -6,9 +6,16 @@ const App = () => {
   const APP_ID = "d3fe2ead";
   const APP_KEY = "62f4a658cef756d9b125eec16d55db17";
 
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
