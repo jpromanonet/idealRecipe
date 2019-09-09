3d649b853f26e64e433fddedc649cf68c315258b
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-09-09 09:18:17 -0300

    Update App.js

--- idealrecipe/src/App.js
+++ idealrecipe/src/App.js
@@ -21,7 +21,6 @@ const App = () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
-    console.log(response);
     const data = await response.json();
     setRecipes(data.hits);
   };
